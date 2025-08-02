869b2c631e104d05ba6a9f2b013ae7e59a0adecf
Dieison <dieisonpablo.li@gmail.com>
2025-08-02 18:16:45 -0300

    versão 1.1

---
+++ script.js
@@ -0,0 +1,23 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxqCd1wsk2r31j73iY0RUFW-2rMLhdMnc1osjf_YduIyvPeArJVwyeEQXrwPdX2bjSaYw/exec';
+
+function handleSubmit(event, status, secao) {
+    event.preventDefault();
+    fetch(scriptURL, {
+        method: 'POST',
+        body: JSON.stringify({
+            status: status,
+            secao: secao
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            alert(`Status "${status}" enviado da seção "${secao}"!`);
+        } else {
+            alert("Erro ao enviar. Tente novamente.");
+        }
+    })
+    .catch(error => alert("Erro de conexão: " + error.message));
+}
